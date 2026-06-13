9bd80687ed660d6f741ada664d08b531c169c8e2
Shiven Mhatre <shivenmhatre23@gmail.com>
2026-06-13 21:42:24 +0530

    Integer data types using modifiers 2

---
+++ modifiers_integer_data_type_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    unsigned int var1=0 ;
+    unsigned var2= UINT_MAX ;
+
+    printf("Range of unsigned integer is from : %u to %u", var1, var2);
+    return 0;
+}
