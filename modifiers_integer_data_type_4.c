53e1f617679bfc2d32ece20cf7b12da8d421918d
Shiven Mhatre <shivenmhatre23@gmail.com>
2026-06-13 22:06:59 +0530

    integer data types using modifiers 4

---
+++ modifiers_integer_data_type_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    unsigned short int var1=0;
+    unsigned short int var2=USHRT_MAX ;
+
+    printf("Range of unsigned short integer is from : %u to %u", var1, var2);
+    return 0;
+}
