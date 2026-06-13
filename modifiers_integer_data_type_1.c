36e921e67c3d539f83205dd4efea951b7f395015
Shiven Mhatre <shivenmhatre23@gmail.com>
2026-06-13 21:01:58 +0530

    Integer data types using modifiers 1

---
+++ modifiers_integer_data_type_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    int var1=INT_MAX;
+    int var2=INT_MIN;
+    printf("Range of signed integer is from : %d to %d" , var1, var2);
+        return 0;
+}
