9f8f2f0e7fb02b070a786c2c5b5e1234cd2f98d5
Shiven Mhatre <shivenmhatre23@gmail.com>
2026-06-23 11:53:40 +0530

    Using all basic arithmetic operators in program

---
+++ arithmetic_operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the first number\n");
+    scanf("%d",&a);
+    printf("Enter the second number\n");
+    scanf("%d",&b);
+    printf("a+b=%d\n",a+b);
+    printf("a-b=%d\n",a-b);
+    printf("a*b=%d\n",a*b);
+    printf("a/b=%d\n",a/b); 
+    printf("a%%b=%d\n",a%b);
+    return 0;
+}
